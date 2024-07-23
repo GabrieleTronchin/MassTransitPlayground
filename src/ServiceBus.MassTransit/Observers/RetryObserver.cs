0bedb47608436791dca97ae07760646e92891eec
Gabriele Tronchin <gabriele.tronchin@reti.it>
2024-07-23 17:59:54 +0200

    Add observer

--- src/ServiceBus.MassTransit/Observers/RetryObserver.cs
+++ src/ServiceBus.MassTransit/Observers/RetryObserver.cs
@@ -2,31 +2,32 @@
 
 namespace MassTransit.Playground.Receivers.Observers
 {
+    //TODO This class is to test
     internal class RetryObserver(ILogger<RetryObserver> logger) : IRetryObserver
     {
         Task IRetryObserver.PostCreate<T>(RetryPolicyContext<T> context)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
 
         Task IRetryObserver.PostFault<T>(RetryContext<T> context)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
 
         Task IRetryObserver.PreRetry<T>(RetryContext<T> context)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
 
         Task IRetryObserver.RetryComplete<T>(RetryContext<T> context)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
 
         Task IRetryObserver.RetryFault<T>(RetryContext<T> context)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
--- src/ServiceBus.Playground.Sender/Observer/BusObserver.cs
+++ src/ServiceBus.Playground.Sender/Observer/BusObserver.cs
@@ -1,44 +1,63 @@
-﻿namespace MassTransit.Playground.Sender.Observer;
+﻿using Microsoft.Extensions.Logging;
 
-internal class BusObserver : IBusObserver
+namespace MassTransit.Playground.Sender.Observer;
+
+internal class BusObserver(ILogger<BusObserver> logger) : IBusObserver
 {
     public void CreateFaulted(Exception exception)
     {
-        throw new NotImplementedException();
+        logger.LogTrace($"A new fault has been created. Message:{exception.Message}");
     }
 
     public void PostCreate(IBus bus)
     {
-        throw new NotImplementedException();
+        logger.LogTrace(
+            $"Service bus created.Topology: {bus.Topology} - Service Bus Type: {bus.Address}"
+        );
     }
 
     public Task PostStart(IBus bus, Task<BusReady> busReady)
     {
-        throw new NotImplementedException();
+        logger.LogTrace(
+            $"Service bus started.Topology: {bus.Topology} - Service Bus Type: {bus.Address}"
+        );
+
+        return Task.CompletedTask;
     }
 
     public Task PostStop(IBus bus)
     {
-        throw new NotImplementedException();
+        logger.LogTrace(
+            $"Service bus Stopped.Topology: {bus.Topology} - Service Bus Type: {bus.Address}"
+        );
+        return Task.CompletedTask;
     }
 
     public Task PreStart(IBus bus)
     {
-        throw new NotImplementedException();
+        logger.LogTrace(
+            $"Service bus pre start.Topology: {bus.Topology} - Service Bus Type: {bus.Address}"
+        );
+        return Task.CompletedTask;
     }
 
     public Task PreStop(IBus bus)
     {
-        throw new NotImplementedException();
+        logger.LogTrace(
+            $"Service bus pre stop.Topology: {bus.Topology} - Service Bus Type: {bus.Address}"
+        );
+        return Task.CompletedTask;
     }
 
     public Task StartFaulted(IBus bus, Exception exception)
     {
-        throw new NotImplementedException();
+        logger.LogTrace($"Start Faulted with Message:{exception.Message}");
+        return Task.CompletedTask;
     }
 
     public Task StopFaulted(IBus bus, Exception exception)
     {
-        throw new NotImplementedException();
+        logger.LogTrace($"Stop Faulted with Message:{exception.Message}");
+        return Task.CompletedTask;
     }
 }
